2375b63c91e346cbbaa5ff26141139d9afcd19e4
pliota01 <pliota01@cs.ucy.ac.cy>
2023-01-11 19:48:06 +0100

    Simple design for activity_send_package.xml and activity_track_package.xml. Buttons can switch between the activities

---
+++ app/src/main/java/nl/rug/cs/pasd/team43/disruptivedelivery/LoginActivity.java
@@ -0,0 +1,14 @@
+package nl.rug.cs.pasd.team43.disruptivedelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/nl/rug/cs/pasd/team43/disruptivedelivery/MainActivity.java
+++ app/src/main/java/nl/rug/cs/pasd/team43/disruptivedelivery/MainActivity.java
@@ -2,19 +2,39 @@ package nl.rug.cs.pasd.team43.disruptivedelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnSendPackage = new Button(this);
-        btnSendPackage = findViewById(R.id.btn_send_package);
+        Button btnSendPackage = (Button) findViewById(R.id.btn_send_package);
+        btnSendPackage.setOnClickListener(this);
+        Button btnTrackPackage = (Button) findViewById(R.id.btn_track_package);
+        btnTrackPackage.setOnClickListener(this);
+
+//        TODO Check if user is a delivery driver and if so add more stuff like the maps and
+//         also see where there have been requests for package pickups
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btn_send_package) {
+            Intent intent = new Intent(this, SendPackageActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.btn_track_package) {
+            Intent intent = new Intent(this, TrackPackageActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
